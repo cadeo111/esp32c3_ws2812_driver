07d6316eb608a672b2df4822629290a12e553554
Caden Keese <caden@keese.dev>
2026-03-25 13:59:10 +0100

    working

---
+++ src/bin/main.rs
@@ -0,0 +1,179 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use embassy_executor::Spawner;
+use embassy_time::{
+    Duration,
+    Timer,
+};
+use esp_backtrace as _;
+use esp_hal::{
+    clock::CpuClock,
+    timer::timg::TimerGroup,
+};
+use log::info;
+use ws2812_driver::neopixel::{
+    LedStrip,
+    LedStripEsp32C3,
+    Rgb,
+    SignalPeriod,
+    min_length_times_24_plus_one,
+};
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
+    // generator version: 1.2.0
+
+    // fix printing problem in wokwi
+    logger::init_custom_logger(log::LevelFilter::Info);
+    // esp_println::logger::init_logger_from_env();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let sw_interrupt =
+        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+    info!("Embassy initialized!");
+
+    const LED_LENGTH: usize = 64;
+    let mut strip: LedStripEsp32C3<'_, LED_LENGTH, { min_length_times_24_plus_one(LED_LENGTH) }> =
+        LedStripEsp32C3::new(peripherals.GPIO6, peripherals.RMT).unwrap();
+    strip.set_led(0, Rgb::RED).unwrap();
+    strip.refresh().unwrap();
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+    let mut idx = 0;
+    let mut step = 0;
+    let max_step = 360;
+    info!("Hello world!");
+    loop {
+        strip.clear();
+        for _ in 0..10 {
+            strip
+                .set_led(idx, Rgb::rainbow_progression(step, max_step))
+                .unwrap();
+            step += 1;
+            step %= max_step;
+            strip.refresh().unwrap();
+            Timer::after(Duration::from_millis(5)).await;
+        }
+        info!("Color: {}", Rgb::rainbow_progression(step, max_step));
+
+        // strip.refresh().unwrap();
+        idx += 1;
+        idx %= LED_LENGTH;
+        // Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
+}
+
+mod logger {
+    use esp_println::print;
+    use log::{
+        LevelFilter,
+        Metadata,
+        Record,
+    };
+
+    // 1. Define an empty struct for your logger
+    struct CustomEspLogger;
+
+    const TIME_START: embassy_time::Instant = embassy_time::Instant::MIN;
+
+    // 2. Implement the standard `log::Log` trait
+    impl log::Log for CustomEspLogger {
+        fn enabled(&self, _metadata: &Metadata) -> bool {
+            // You can add more complex filtering here if you want
+            true
+        }
+
+        fn log(&self, record: &Record) {
+            if self.enabled(record.metadata()) {
+                // Use `print!` instead of `println!` so we can manually add \r\n
+                // print!(
+                //     "[{}] {}: {}\r\n",
+                //     record.level(),
+                //     record.target(),
+                //     record.args()
+                // );
+                print_log_record(record)
+            }
+        }
+
+        fn flush(&self) {}
+    }
+
+    const COLOR_ENABLED: bool = true;
+
+    fn print_log_record(record: &log::Record) {
+        let (color, reset) = if COLOR_ENABLED {
+            const RESET: &str = "\u{001B}[0m";
+            const RED: &str = "\u{001B}[31m";
+            const GREEN: &str = "\u{001B}[32m";
+            const YELLOW: &str = "\u{001B}[33m";
+            const BLUE: &str = "\u{001B}[34m";
+            const CYAN: &str = "\u{001B}[35m";
+
+            let color = match record.level() {
+                log::Level::Error => RED,
+                log::Level::Warn => YELLOW,
+                log::Level::Info => GREEN,
+                log::Level::Debug => BLUE,
+                log::Level::Trace => CYAN,
+            };
+            let reset = RESET;
+            (color, reset)
+        } else {
+            ("", "")
+        };
+
+        let now = TIME_START.elapsed();
+        let total_secs = now.as_secs();
+
+        // let hours = total_secs / 3600;
+        let minutes = (total_secs % 3600) / 60;
+        let seconds = total_secs % 60;
+        let millis = now.as_millis() % 1000;
+
+        print!(
+            "{}{} ({:02}:{:02}.{:03}) - {}{}\n\r",
+            color,
+            record.level(),
+            minutes,
+            seconds,
+            millis,
+            record.args(),
+            reset
+        );
+    }
+
+    // 3. Create a static instance of the logger
+    static LOGGER: CustomEspLogger = CustomEspLogger;
+
+    // 4. Create an init function to bind it to the `log` crate
+    pub fn init_custom_logger(level: LevelFilter) {
+        unsafe {
+            log::set_logger_racy(&LOGGER).unwrap();
+            log::set_max_level_racy(level);
+        }
+    }
+}
---
+++ src/neopixel/strip_trait.rs
@@ -0,0 +1,126 @@
+use core::time::Duration;
+
+use heapless::Vec;
+use thiserror::Error;
+
+pub trait Color24bit: Sized {
+    fn as_24_bit_color_u32(&self) -> u32;
+}
+
+#[derive(Error, Debug)]
+pub enum LedStripTraitError {
+    #[error("index {index} out of range  of length {length}")]
+    IndexOutOfRangeOfStrip { length: usize, index: usize },
+    #[error("signal vector was too small for transmission")]
+    SignalVectorTooSmall,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, Default)]
+pub struct SignalPeriod {
+    high: Duration,
+    low: Duration,
+}
+impl SignalPeriod {
+    pub const fn new(high: Duration, low: Duration) -> Self {
+        Self { high, low }
+    }
+    pub fn high(&self) -> Duration {
+        self.high
+    }
+    pub fn low(&self) -> Duration {
+        self.low
+    }
+}
+
+pub const fn times_24(length: usize) -> usize {
+    length * 24 + 1
+}
+
+/// MIN_SIGNAL_LENGTH should be at least LENGTH *24
+/// _methods are internal, should be avoided
+pub trait LedStrip<const LENGTH: usize, const MIN_SIGNAL_LENGTH: usize, C: Color24bit> {
+    type Error: core::error::Error + From<LedStripTraitError>;
+    type SignalPeriodType;
+
+    const LOGIC_0: SignalPeriod =
+        SignalPeriod::new(Duration::from_nanos(350), Duration::from_nanos(800));
+    const LOGIC_1: SignalPeriod =
+        SignalPeriod::new(Duration::from_nanos(700), Duration::from_nanos(600));
+
+    fn clear(&mut self) {
+        for i in 0..LENGTH {
+            self._zero_out_index_unchecked(i);
+        }
+    }
+    fn _zero_out_index_unchecked(&mut self, index: usize);
+    fn _set_led_unchecked(&mut self, index: usize, color: C);
+    fn set_led(&mut self, index: usize, color: C) -> core::result::Result<(), Self::Error> {
+        if index >= LENGTH {
+            Err(LedStripTraitError::IndexOutOfRangeOfStrip {
+                length: LENGTH,
+                index,
+            })?;
+        }
+        self._set_led_unchecked(index, color);
+        Ok(())
+    }
+
+    fn get_led(&self, index: usize) -> core::result::Result<C, Self::Error> {
+         if index >= LENGTH {
+            Err(LedStripTraitError::IndexOutOfRangeOfStrip {
+                length: LENGTH,
+                index,
+            })?;
+        }
+        Ok(self._get_led_unchecked(index))
+    }
+
+    fn _get_led_unchecked(&self, index: usize) -> C; 
+
+    fn _get_periods(
+        &mut self,
+    ) -> core::result::Result<heapless::Vec<Self::SignalPeriodType, MIN_SIGNAL_LENGTH>, Self::Error>
+    {
+        const {
+            assert!(
+                MIN_SIGNAL_LENGTH >= LENGTH * 24,
+                "Buffer size must be at least 24 x LENGTH (the number of leds) !"
+            );
+        }
+
+        let mut signal = Vec::<Self::SignalPeriodType, MIN_SIGNAL_LENGTH>::new();
+
+        let mut s = [SignalPeriod::default(); 24];
+    
+        for index in 0..LENGTH {
+            let c = self.get_led(index)?;
+            let color: u32 = c.as_24_bit_color_u32();
+            for i in (0..24).rev() {
+                let p = 2_u32.pow(i);
+                let bit: bool = (p & color) != 0;
+                s[(23 - i) as usize] = if bit { Self::LOGIC_1 } else { Self::LOGIC_0 };
+            }
+            for item in s {
+                signal
+                    .push(self._convert_from_signal_period(item)?)
+                    .map_err(|_| LedStripTraitError::SignalVectorTooSmall)?;
+            }
+        }
+        Ok(signal)
+    }
+
+    fn _convert_from_signal_period(
+        &self,
+        p: SignalPeriod,
+    ) -> core::result::Result<Self::SignalPeriodType, Self::Error>;
+
+    fn _transmit_signal(
+        &mut self,
+        signal: heapless::Vec<Self::SignalPeriodType, MIN_SIGNAL_LENGTH>,
+    ) -> core::result::Result<(), Self::Error>;
+
+    fn refresh(&mut self) -> core::result::Result<(), Self::Error> {
+        let signal = self._get_periods()?;
+        self._transmit_signal(signal)
+    }
+}
